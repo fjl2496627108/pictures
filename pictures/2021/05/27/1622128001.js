33da1718f14d056d82a6cf3e45fc7f4ee132dc18
无垠 <72548546+fjl2496627108@users.noreply.github.com>
2021-05-27 23:06:41 +0800

    upload by GithubStatic

---
+++ pictures/2021/05/27/1622128001.js
@@ -0,0 +1,8 @@
+window.paceOptions = {
+            ajax: {
+                ignoreURLs: ['/.*handsome-meting-api.*/']
+            },//忽视音乐加载的ajax请求
+            restartOnPushState: false,
+            startOnPageLoad: false,
+            restartOnRequestAfter: false
+        };
